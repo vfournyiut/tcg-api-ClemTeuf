77d73a78847aec6f7070d5b5b6b85d2cb8be8334
Clément DEQUIDT <dequidtclement1@gmail.com>
2026-01-29 19:03:00 +0100

    Modification middleware #2

--- src/auth.middleware.ts
+++ src/auth.middleware.ts
@@ -24,7 +24,7 @@ export const authentificateToken = (
         return res.status(401).json({error : 'Token manquant.'})
     }
 
-    const token = authHeader.split(' ')[1]
+    const token = authHeader && authHeader.split(' ')[1]
 
     if(!token) {
         return res.status(401).json({error : 'Token invalide.'})
